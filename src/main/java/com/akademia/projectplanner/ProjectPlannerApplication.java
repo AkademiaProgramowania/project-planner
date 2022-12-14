eee00af78707678b2c200577910f857f690ae909
jszymczak <jszymczak@xtm-intl.com>
2022-12-14 16:36:31 +0100

    Initial commit

---
+++ src/main/java/com/akademia/projectplanner/ProjectPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.akademia.projectplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akademia/projectplanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akademia.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
