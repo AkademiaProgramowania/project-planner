e2b68e23d2c4d64da6613c686b63cbdd13ffd0f7
jszymczak <asiaszymczak9@gmail.com>
2023-01-22 17:49:08 +0100

    Add TaskMapper and convert Task where necessary

--- src/main/java/com/akademia/projectplanner/dto/TaskDto.java
+++ src/main/java/com/akademia/projectplanner/dto/TaskDto.java
@@ -1,13 +1,17 @@
 package com.akademia.projectplanner.dto;
 
+import com.akademia.projectplanner.entity.UserEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class TaskDto {
+
+  private Long id;
 
   @NotEmpty(message = "Task name cannot be empty!")
   private String name;
@@ -15,4 +19,6 @@ public class TaskDto {
   private String status;
   private String description;
   private String deadline;
+  private LocalDate startDate;
+  private UserEntity userEntity;
 }
--- src/main/java/com/akademia/projectplanner/service/TaskService.java
+++ src/main/java/com/akademia/projectplanner/service/TaskService.java
@@ -3,6 +3,7 @@ package com.akademia.projectplanner.service;
 import com.akademia.projectplanner.dto.TaskDto;
 import com.akademia.projectplanner.exception.TaskDoesNotExistException;
 import com.akademia.projectplanner.entity.TaskEntity;
+import com.akademia.projectplanner.mapper.TaskMapper;
 import com.akademia.projectplanner.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,13 @@ import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class TaskService {
 
+  private TaskMapper taskMapper;
   private TaskRepository taskRepository;
 
   public void addTask(TaskDto taskDto) {
@@ -25,12 +28,9 @@ public class TaskService {
     if (!checkIfDatesAreValid(taskDto.getDeadline())) {
       throw new DateTimeException("Invalid date selected!");
     }
-    TaskEntity taskEntity =
-        new TaskEntity(
-            taskDto.getName(),
-            taskDto.getStatus(),
-            taskDto.getDescription(),
-            taskDto.getDeadline());
+
+    taskDto.setStartDate(LocalDate.now());
+    TaskEntity taskEntity = taskMapper.toTaskEntity(taskDto);
     taskRepository.save(taskEntity);
   }
 
@@ -42,12 +42,20 @@ public class TaskService {
     return deadline.isBlank() || LocalDate.now().isBefore(LocalDate.parse(deadline));
   }
 
-  public List<TaskEntity> getAllTasks() {
-    return taskRepository.findAll();
+  public List<TaskDto> getAllTasks() {
+
+    return taskRepository.findAll().stream()
+        .map(taskEntity -> taskMapper.toTaskDto(taskEntity))
+        .collect(Collectors.toList());
   }
 
-  public TaskEntity getTaskInfo(Long taskId) {
+  public TaskDto getTaskInfo(Long taskId) {
     Optional<TaskEntity> taskOptional = taskRepository.findById(taskId);
-    return taskOptional.orElseThrow(() -> new TaskDoesNotExistException("Task does not exist!"));
+    if (!taskOptional.isPresent()) {
+      throw new TaskDoesNotExistException("Task does not exist!");
+    } else {
+      TaskEntity taskEntity = taskOptional.get();
+      return taskMapper.toTaskDto(taskEntity);
+    }
   }
 }
