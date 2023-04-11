f8854e09cf3ce542fd8dbced90a31000efdca208
Agnieszkaad77 <105515267+Agnieszkaad77@users.noreply.github.com>
2023-04-11 18:23:28 +0200

    User selection added to add/edit task (#23)

--- src/main/java/com/akademia/projectplanner/service/RegistrationService.java
+++ src/main/java/com/akademia/projectplanner/service/RegistrationService.java
@@ -9,6 +9,9 @@ import com.akademia.projectplanner.validation.RegistrationValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class RegistrationService {
@@ -26,5 +29,11 @@ public class RegistrationService {
     }
     UserEntity userEntity = userMapper.toUserEntity(userDto);
     userRepository.save(userEntity);
+  }
+
+  public List<UserDto> getAllUsers() {
+    return userRepository.findAll().stream()
+        .map(userEntity -> userMapper.toUserDto(userEntity))
+        .collect(Collectors.toList());
   }
 }
