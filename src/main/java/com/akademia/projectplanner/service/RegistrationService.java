a2b34b93fed59875bf5c91a2496e4c1b4d94cade
Agnieszka <agnieszka.ad77@gmail.com>
2023-04-19 18:26:00 +0200

    Code formatted.

--- src/main/java/com/akademia/projectplanner/service/RegistrationService.java
+++ src/main/java/com/akademia/projectplanner/service/RegistrationService.java
@@ -4,23 +4,21 @@ import com.akademia.projectplanner.dto.UserDto;
 
 import java.util.List;
 
-/**
- * The interface used to communication with RegistrationServiceImpl.
- */
+/** The interface used to communication with RegistrationServiceImpl. */
 public interface RegistrationService {
 
-    /**
-     * Saves newly created user in the database. Method throws IllegalArgumentException if mandatory fields are not
-     * filled in, or AuthenticationException if email or password is not correct.
-     *
-     * @param userDto the UserDto object that contains the form data submitted by the user
-     */
-    void register(UserDto userDto);
+  /**
+   * Saves newly created user in the database. Method throws IllegalArgumentException if mandatory
+   * fields are not filled in, or AuthenticationException if email or password is not correct.
+   *
+   * @param userDto the UserDto object that contains the form data submitted by the user
+   */
+  void register(UserDto userDto);
 
-    /**
-     * Finds all users in database and maps UserEntity to UserDto.
-     *
-     * @return the list of UserDto.
-     */
-    List<UserDto> getAllUsers();
+  /**
+   * Finds all users in database and maps UserEntity to UserDto.
+   *
+   * @return the list of UserDto.
+   */
+  List<UserDto> getAllUsers();
 }
--- src/main/java/com/akademia/projectplanner/service/TaskService.java
+++ src/main/java/com/akademia/projectplanner/service/TaskService.java
@@ -4,31 +4,30 @@ import com.akademia.projectplanner.dto.TaskDto;
 
 import java.util.List;
 
-/**
- * The interface used to communication with TaskServiceImpl.
- */
+/** The interface used to communication with TaskServiceImpl. */
 public interface TaskService {
 
-    /**
-     * Saves newly created user in the database. Method throws IllegalArgumentException if mandatory fields are not
-     * filled in, or DateTimeException if invalid date is selected.
-     * @param taskDto The task object to add.
-     */
-    void addTask(TaskDto taskDto);
+  /**
+   * Saves newly created user in the database. Method throws IllegalArgumentException if mandatory
+   * fields are not filled in, or DateTimeException if invalid date is selected.
+   *
+   * @param taskDto The task object to add.
+   */
+  void addTask(TaskDto taskDto);
 
-    /**
-     * Finds all tasks in database and maps TaskEntity to TaskDto.
-     *
-     * @return The list of TaskDto.
-     */
-    List<TaskDto> getAllTasks();
+  /**
+   * Finds all tasks in database and maps TaskEntity to TaskDto.
+   *
+   * @return The list of TaskDto.
+   */
+  List<TaskDto> getAllTasks();
 
-    /**
-     * Finds particular task by id and maps TaskEntity to TaskDto. It throws TaskDoesNotExistException
-     * if task is not found.
-     *
-     * @param taskId the ID of the task to be found.
-     * @return found TaskDto
-     */
-    TaskDto getTaskInfo(Long taskId);
+  /**
+   * Finds particular task by id and maps TaskEntity to TaskDto. It throws TaskDoesNotExistException
+   * if task is not found.
+   *
+   * @param taskId the ID of the task to be found.
+   * @return found TaskDto
+   */
+  TaskDto getTaskInfo(Long taskId);
 }
