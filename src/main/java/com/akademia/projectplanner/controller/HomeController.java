e0afe0360b44b4037576b27cc8b81fd422e1c18d
Joanna <63641651+fngs101@users.noreply.github.com>
2023-06-18 19:20:27 +0200

    Create a map task-user and send it to the model to display user email… (#32)
    * Create a map task-user and send it to the model to display user email under the task name
    
    * Refactor the map to have a string for email instead of whole user
    
    * Rename the map to better represent the insides of it

--- src/main/java/com/akademia/projectplanner/controller/HomeController.java
+++ src/main/java/com/akademia/projectplanner/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Controller class that handles requests for the home page of the application. The rendered view of
@@ -31,11 +32,13 @@ public class HomeController {
    */
   @GetMapping({"/", "index"})
   public String getMainPage(Model model) {
+
     List<TaskDto> allTasks = taskService.getAllTasks();
+    Map<TaskDto, String> tasksWithEmails = taskService.createTasksEmailsMap(allTasks);
     UserDetails userDetails =
         (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     model.addAttribute("userName", userDetails.getUsername());
-    model.addAttribute("tasks", allTasks);
+    model.addAttribute("tasksWithEmails", tasksWithEmails);
 
     return "index";
   }
